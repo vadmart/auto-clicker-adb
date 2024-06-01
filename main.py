4c6c5b27ee88122e988de9c87f93d2734b7124fa
vadjango <z9661744@gmail.com>
2024-06-01 14:03:31 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import logging
+import random
+import subprocess
+import multiprocessing as mp
+import time
+from datetime import datetime, timedelta
+
+logger = logging.getLogger(__name__)
+
+adb_commands = [
+    "-s {device} shell sendevent /dev/input/event4 3 53 15079",
+    "-s {device} shell sendevent /dev/input/event4 3 54 19612",
+    "-s {device} shell sendevent /dev/input/event4 0 2 0",
+    "-s {device} shell sendevent /dev/input/event4 0 0 0",
+    "-s {device} shell sendevent /dev/input/event4 0 2 0",
+    "-s {device} shell sendevent /dev/input/event4 0 0 0",
+]
+
+
+def adb(command: str, capture_output: bool = True):
+    return subprocess.run(f"adb {command}", capture_output=capture_output)
+
+
+def get_devices_with_resolutions():
+    result = adb("devices").stdout.decode("utf-8").split()
+    devices = [[el] for el in result if el not in ("List", "of", "devices", "attached", "device")]
+    for i in range(len(devices)):
+        resolution = adb(f"-s {devices[i][0]} shell wm size").stdout.decode("utf-8").split()[-1].split("x")
+        devices[i].extend([int(resolution[0]), int(resolution[1])])
+    return devices
+
+
+def start():
+    logger.info("Getting devices...")
+    devices_with_resolutions = get_devices_with_resolutions()
+    logger.info(f"Devices' list: {devices_with_resolutions}")
+    start_time = datetime.now()
+    while True:
+        for device_with_resolutions in devices_with_resolutions:
+            x, y = (random.randint(device_with_resolutions[1] // 2.4, device_with_resolutions[1] // 1.44),
+                    random.randint(device_with_resolutions[2] // 1.4, device_with_resolutions[2] // 1.34))
+            subprocess.Popen(["adb", "-s", device_with_resolutions[0], "shell", "input", "tap", str(x), str(y)])
+            time.sleep(0.05)
+        if datetime.now() - start_time >= timedelta(minutes=2, seconds=10):
+            time.sleep(670)
+
+
+if __name__ == "__main__":
+    start()
